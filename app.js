0dc9d99a57ee21fe7b34d9035e505c2d9f042aba
Claudiu Sonica <csonica95@gmail.com>
2023-05-26 20:01:24 +0200

    server structure complete

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
