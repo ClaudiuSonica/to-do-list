c2d5c9c1f6659a3cb38ea8ff74a34843546c592b
Claudiu Sonica <csonica95@gmail.com>
2023-05-27 19:20:14 +0200

    basic functionality

--- app.js
+++ app.js
@@ -4,31 +4,32 @@ const port = 3000;
 
 const app = express();
 
+let items = ["Prepare your coffee", "Start the PC", "Start coding"];
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  
-  let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (currentDay === 6 || currentDay === 0) {
-    day = "Weekend";
-    res.send("weekend");
-  } else {
-    day = "Weekday";
-    res.send("weekday");
-  }
+app.get("/", (req, res) => {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
+app.post("/", (req, res) => {
 
+  let newItem = req.body.newItem;
 
-
-
-
-
-
-
+  items.push(newItem);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
